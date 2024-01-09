e6aa09eccf2481daea39a534dbff67780139bd1c
Botond Lajos Perényi <halkszavu@outlook.com>
2024-01-09 16:16:33 +0100

    GetItinerary(start, end) implemented

--- RestAPI/Bll/Services/Impl/ItineraryService.cs
+++ RestAPI/Bll/Services/Impl/ItineraryService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Bll.Dtos;
+using DAL;
+using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,72 @@ using System.Threading.Tasks;
 
 namespace Bll.Services.Impl
 {
-	public class ItineraryService
+	public class ItineraryService : IItineraryService
 	{
+		private readonly TransportationContext transportationContext;
+
+		public ItineraryService(TransportationContext transportationContext)
+		{
+			this.transportationContext = transportationContext;
+		}
+
+		public ItineraryDto CreateNewItinerary(ItineraryDto itinerary)
+		{
+			Itinerary entity = new Itinerary()
+			{
+				Start = itinerary.Start,
+				End = itinerary.End,
+			};
+
+			transportationContext.Itineraries.Add(entity);
+			transportationContext.SaveChanges();
+			return new ItineraryDto()
+			{
+				ID = entity.ID,
+				Start = entity.Start,
+				End = entity.End,
+			};
+		}
+
+		public IEnumerable<ItineraryDto> GetItinerary(int userId)
+		{
+			return new List<ItineraryDto>();
+		}
+
+		public IEnumerable<ItineraryDto> GetItinerary(string start, string end)
+		{
+			if(string.IsNullOrEmpty(end))
+			{
+				return transportationContext.Itineraries.Where(i => i.Start == start).Select(i => new ItineraryDto()
+				{
+					ID = i.ID,
+					Start = i.Start,
+					End = i.End,
+				});
+			}
+			else if(string.IsNullOrEmpty(start))
+			{
+				return transportationContext.Itineraries.Where(i => i.End == end).Select(i => new ItineraryDto()
+				{
+					ID = i.ID,
+					Start = i.Start,
+					End = i.End,
+				});
+			}
+			else
+			{
+				return transportationContext.Itineraries.Where(i => i.Start == start && i.End == end).Select(i => new ItineraryDto()
+				{
+					ID = i.ID,
+					Start = i.Start,
+					End = i.End,
+				});
+			}
+		}
+
+		public void UpdateItinerary(int id, ItineraryDto itinerary)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
