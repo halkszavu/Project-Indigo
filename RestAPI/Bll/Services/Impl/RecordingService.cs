0161f79664caec5249266cb4f330cf987f6e869b
Botond Lajos Perényi <halkszavu@outlook.com>
2024-01-09 16:21:09 +0100

    scaffolded

--- RestAPI/Bll/Services/Impl/RecordingService.cs
+++ RestAPI/Bll/Services/Impl/RecordingService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bll.Dtos;
+using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,28 @@ using System.Threading.Tasks;
 
 namespace Bll.Services.Impl
 {
-	public class RecordingService
+	public class RecordingService : IRecordingService
 	{
+		private readonly TransportationContext transportationContext;
+
+		public RecordingService(TransportationContext transportationContext)
+		{
+			this.transportationContext = transportationContext;
+		}
+
+		public RecordingDto CreateNewRecording(RecordingDto recording)
+		{
+			throw new NotImplementedException();
+		}
+
+		public RecordingDto GetRecording(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<RecordingDto> GetRecordings()
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
