632791af8c0c1ed2310270b4865aefc5bf089fb0
Botond Lajos Perényi <halkszavu@outlook.com>
2024-01-18 11:06:42 +0100

    8E done

--- RestAPI/DAL/Migrations/20240117103704_Seeding2.cs
+++ RestAPI/DAL/Migrations/20240117103704_Seeding2.cs
@@ -52,8 +52,8 @@ namespace DAL.Migrations
                     { 281, "BAH-csomópont" },
                     { 282, "Mészáros utca" },
                     { 283, "Sánc utca" },
-                    { 263, "" },
-                    { 263, "" },
+                    { 284, "Nagyszeben tér" },
+                    { 285, "Jégvirág utca" },
                     { 263, "" },
                     { 263, "" },
                     { 263, "" },
@@ -114,23 +114,52 @@ namespace DAL.Migrations
                     { "Outbound", 39, 21, 37 },
                     { "Outbound", 40, 21, 38 },
                     { "Outbound", 41, 21, 39 },
-					{ "Inbound", 1, 21, 261 }
-                });
+					{ "Inbound", 1, 21, 39 },
+                    { "Inbound", 2, 21, 38 },
+                    { "Inbound", 3, 21, 37 },
+                    { "Inbound", 4, 21, 35 },
+                    { "Inbound", 5, 21, 34 },
+                    { "Inbound", 6, 21, 33 },
+                    { "Inbound", 7, 21, 30 },
+                    { "Inbound", 8, 21, 29 },
+                    { "Inbound", 9, 21, 28 },
+                    { "Inbound", 10, 21, 24 },
+                    { "Inbound", 11, 21, 22 },
+                    { "Inbound", 12, 21, 21 },
+                    { "Inbound", 13, 21, 20 },
+                    { "Inbound", 14, 21, 19 },
+                    { "Inbound", 15, 21, 18 },
+                    { "Inbound", 16, 21, 241 },
+                    { "Inbound", 17, 21, 283 },
+                    { "Inbound", 18, 21, 282 },
+                    { "Inbound", 19, 21, 281 },
+                    { "Inbound", 20, 21, 280 },
+                    { "Inbound", 21, 21, 279 },
+                    { "Inbound", 22, 21, 278 },
+                    { "Inbound", 23, 21, 277 },
+                    { "Inbound", 24, 21, 276 },
+                    { "Inbound", 25, 21, 275 },
+                    { "Inbound", 26, 21, 274 },
+                    { "Inbound", 27, 21, 273 },
+                    { "Inbound", 28, 21, 272 },
+                    { "Inbound", 29, 21, 271 },
+                    { "Inbound", 30, 21, 270 },
+                    { "Inbound", 31, 21, 269 },
+                    { "Inbound", 32, 21, 268 },
+                    { "Inbound", 33, 21, 267 },
+                    { "Inbound", 34, 21, 266 },
+                    { "Inbound", 35, 21, 265 },
+                    { "Inbound", 36, 21, 264 },
+                    { "Inbound", 37, 21, 284 },
+                    { "Inbound", 38, 21, 263 },
+                    { "Inbound", 39, 21, 285 },
+                    { "Inbound", 40, 21, 96 },
+				});
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DeleteData(
-                table: "RouteStops",
-                keyColumn: "ID",
-                keyValue: 780);
-
-            migrationBuilder.DeleteData(
-                table: "RouteStops",
-                keyColumn: "ID",
-                keyValue: 781);
-
             migrationBuilder.DeleteData(
                 table: "Routes",
                 keyColumn: "ID",
